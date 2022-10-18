fd74ee9807a78288d77584540f12f709de41fb13
kartik <kartik.varanasi@yahoo.com>
2022-10-19 03:23:38 +0530

    Modal login/Register code refactored

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,32 +2,32 @@ import React, { useState } from "react";
 import Login from "../login/Login";
 import Register from "../register/Register";
 const Modal = () => {
-	const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
 
-	return (
-		<>
-			<input type="checkbox" id="my-modal-4" className="modal-toggle" />
-			<label htmlFor="my-modal-4" className="modal cursor-pointer">
-				<label className="relative w-96 ">
-					<div className="absolute top-6 tabs">
-						<button
-							className={`tab tab-lifted ${isLogin ? "tab-active" : null}`}
-							onClick={() => setIsLogin(true)}
-						>
-							Login
-						</button>
-						<button
-							className={`tab tab-lifted ${isLogin ? null : "tab-active"}`}
-							onClick={() => setIsLogin(false)}
-						>
-							Register
-						</button>
-					</div>
-					{isLogin ? <Login /> : <Register />}
-				</label>
-			</label>
-		</>
-	);
+  return (
+    <>
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+        <label className="relative w-96 ">
+          <div className="absolute top-6 tabs tabs-boxed w-full">
+            <button
+              className={`tab tab-md w-[50%] ${isLogin ? "tab-active" : null}`}
+              onClick={() => setIsLogin(true)}
+            >
+              Login
+            </button>
+            <button
+              className={`tab  tab-md w-[50%] ${isLogin ? null : "tab-active"}`}
+              onClick={() => setIsLogin(false)}
+            >
+              Register
+            </button>
+          </div>
+          {isLogin ? <Login /> : <Register />}
+        </label>
+      </label>
+    </>
+  );
 };
 
 export default Modal;
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -2,81 +2,69 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router-dom";
 const Register = () => {
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log("LOGGING IN...");
-	};
-	return (
-		<>
-			<div className="py-6">
-				<div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-4xl">
-					<div className="w-full px-8 py-10 ">
-						<p className="text-xl text-gray-600 text-center">REGISTER</p>
-						<a
-							href="#"
-							className="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100"
-						>
-							<div className="px-4 py-3">
-								<FcGoogle size={26} />
-							</div>
-							<h1 className="px-4 py-3 w-5/6 text-center text-gray-600 font-bold">
-								Sign in with Google
-							</h1>
-						</a>
-						<div className="mt-4 flex items-center justify-between">
-							<span className="border-b w-1/5 lg:w-1/4"></span>
-							<a href="#" className="text-xs text-center text-gray-500 uppercase">
-								or login with email
-							</a>
-							<span className="border-b w-1/5 lg:w-1/4"></span>
-						</div>
-						<form onSubmit={handleSubmit}>
-							<div className="mt-4">
-								<label className="block text-gray-700 text-sm font-bold mb-2">
-									Display Name
-								</label>
-								<input
-									className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-									type="text"
-									required
-								/>
-							</div>
-							<div className="mt-4">
-								<label className="block text-gray-700 text-sm font-bold mb-2">
-									Email Address
-								</label>
-								<input
-									className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-									type="email"
-									required
-								/>
-							</div>
-							<div className="mt-4">
-								<div className="flex justify-between">
-									<label className="block text-gray-700 text-sm font-bold mb-2">
-										Password
-									</label>
-								</div>
-								<input
-									className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-									type="password"
-									required
-								/>
-							</div>
-							<div className="mt-8">
-								<button
-									type="submit"
-									className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600"
-								>
-									REGISTER
-								</button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("registering  IN...");
+  };
+  return (
+    <>
+      <div className="py-6">
+        <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-4xl">
+          <div className="w-full px-8 pt-12 pb-6">
+            <p className="text-lg text-gray-600 text-center">
+              Create a new Account
+            </p>
+            <button className="btn btn-sm btn-outline w-full rounded-full mt-4 gap-x-4">
+              <FcGoogle size={22} />
+              Sign in with google
+            </button>
+            <div className="divider text-xs text-gray-400 uppercase">
+              or login with email
+            </div>
+            <form onSubmit={handleSubmit} className="form-control">
+              <div>
+                <label className="label-text font-bold mb-2 block">
+                  Email Address
+                </label>
+                <input
+                  className="input input-bordered w-full border-2"
+                  type="email"
+                  required
+                />
+              </div>
+              <div className="mt-4">
+                <div className="flex justify-between">
+                  <label className="label-text font-bold mb-2">Password</label>
+                </div>
+                <input
+                  className="input input-bordered w-full border-2"
+                  type="password"
+                  required
+                />
+              </div>
+              <div className="mt-4">
+                <div className="flex justify-between">
+                  <label className="label-text font-bold mb-2">
+                    Confirm Password
+                  </label>
+                </div>
+                <input
+                  className="input input-bordered w-full border-2"
+                  type="password"
+                  required
+                />
+              </div>
+              <div className="mt-4">
+                <button type="submit" className="btn w-full">
+                  REGISTER
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
