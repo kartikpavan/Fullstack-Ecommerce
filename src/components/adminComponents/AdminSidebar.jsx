08bc485ac5ce4b68840b68cf7fc573e21ad37a79
kartikpavan <kartik.varanasi@yahoo.com>
2022-10-22 16:55:30 +0530

    Admin Sidebar improved

--- src/components/adminComponents/AdminSidebar.jsx
+++ src/components/adminComponents/AdminSidebar.jsx
@@ -6,36 +6,46 @@ const AdminSidebar = () => {
 	const { userName } = useSelector((store) => store.auth);
 
 	// Active link class
-	const activeLink = ({ isActive }) =>
-		isActive ? "border-b-4 border-blue-500 duration-300 " : "null";
+	let activeStyle = {
+		borderRight: "5px solid #181a2a",
+		display: "block",
+		// padding: "0.5rem 0",
+	};
 	return (
 		<div className="w-full">
 			<div className="w-full h-44 bg-primary flex items-center justify-center flex-col gap-2 text-center">
 				<RiAdminLine size={40} color="white" />
 				<h1 className="text-lg md:text-2xl font-bold text-gray-200">{userName}</h1>
 			</div>
-			<nav>
-				<div className="text-md md:text-xl p-4 duration-300  border-b-2 ">
-					<NavLink to="/admin/home" className={activeLink}>
-						Home
-					</NavLink>
-				</div>
-				<div className="text-md md:text-xl  duration-300  border-b-2   p-4">
-					<NavLink to="/admin/all-products" className={activeLink}>
-						All Products
-					</NavLink>
-				</div>
-				<div className="text-md md:text-xl  duration-300   border-b-2  p-4">
-					<NavLink to="/admin/add-product/ADD" className={activeLink}>
-						Add Products
-					</NavLink>
-				</div>
-				<div className="text-md md:text-xl  duration-300   border-b-2  p-4">
-					<NavLink to="/admin/orders" className={activeLink}>
-						Orders
-					</NavLink>
-				</div>
-			</nav>
+			<div className="text-md md:text-xl py-4 pl-2">
+				<NavLink to="/admin/home" style={({ isActive }) => (isActive ? activeStyle : null)}>
+					Home
+				</NavLink>
+			</div>
+			<div className="text-md md:text-xl py-4 pl-2">
+				<NavLink
+					to="/admin/all-products"
+					style={({ isActive }) => (isActive ? activeStyle : null)}
+				>
+					All Products
+				</NavLink>
+			</div>
+			<div className="text-md md:text-xl py-4  pl-2">
+				<NavLink
+					to="/admin/add-product/ADD"
+					style={({ isActive }) => (isActive ? activeStyle : null)}
+				>
+					Add Products
+				</NavLink>
+			</div>
+			<div className="text-md md:text-xl py-4  pl-2">
+				<NavLink
+					to="/admin/orders"
+					style={({ isActive }) => (isActive ? activeStyle : null)}
+				>
+					Orders
+				</NavLink>
+			</div>
 		</div>
 	);
 };
