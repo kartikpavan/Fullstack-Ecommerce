5834e50db5569ce3d31fb3db3897a790e68ebed4
kartikpavan <kartik.varanasi@yahoo.com>
2022-10-29 21:24:37 +0530

    Improved Pwd reset page alert visibility

--- src/pages/resetPassword/ResetPassword.jsx
+++ src/pages/resetPassword/ResetPassword.jsx
@@ -8,19 +8,21 @@ import Loader from "../../components/loader/Loader";
 const ResetPassword = () => {
 	const [email, setEmail] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
+	const [err, setErr] = useState("");
 	const resetPasswordHandler = (e) => {
 		e.preventDefault();
 		setIsLoading(true);
 		sendPasswordResetEmail(auth, email)
 			.then(() => {
 				toast.info("Check email for reset link");
+				setErr("Check your Registered email address for reset link *(Check Spam)*");
 				setIsLoading(false);
 				setEmail("");
 			})
 			.catch((error) => {
 				const errorCode = error.code;
 				const errorMessage = error.message;
-				toast.error(errorCode, errorMessage);
+				setErr(`${errorCode} : ${errorMessage}`);
 				setIsLoading(false);
 			});
 	};
@@ -28,9 +30,15 @@ const ResetPassword = () => {
 	return (
 		<>
 			{isLoading && <Loader />}
+
 			<main className="w-full page flex items-center justify-center">
 				<div className="w-96 h-auto shadow-xl rounded-md px-4 py-6">
 					<h1 className="text-2xl font-bold text-center ">RESET PASSWORD</h1>
+					{err && (
+						<h1 className="alert shadow-lg text-gray-700 border-l-4 border-error my-4">
+							{err}
+						</h1>
+					)}
 					<div className="alert shadow-lg text-gray-700 border-l-4 border-primary my-4">
 						Please enter your registered Email address. You will receive an email
 						message with instructions on how to reset your password
