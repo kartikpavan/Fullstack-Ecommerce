310237fc384d57ce03c83e69bd0f97c4b8b14e1b
Pavan <kartik.varanasi@yahoo.com>
2024-04-05 19:28:51 +0530

    loader message

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,17 +1,20 @@
 import ReactDOM from "react-dom";
 
 const Loader = () => {
-	return ReactDOM.createPortal(
-		<div className="overlay">
-			<div className="loader"></div>
-		</div>,
-		document.getElementById("loader")
-	);
-	// return (
-	// 	<div className="overlay">
-	// 		<div class="loader"></div>
-	// 	</div>
-	// );
+  return ReactDOM.createPortal(
+    <div className="overlay">
+      <div>
+        <h1 className="text-xl font-semibold"> Please wait , This might take a while</h1>
+        <div className="loader"></div>
+      </div>
+    </div>,
+    document.getElementById("loader")
+  );
+  // return (
+  // 	<div className="overlay">
+  // 		<div class="loader"></div>
+  // 	</div>
+  // );
 };
 
 export default Loader;
